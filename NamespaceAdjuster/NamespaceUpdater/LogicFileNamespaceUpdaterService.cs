7543e52390a8286cbdf8462fa438f13ac662cbe0
Eric <erikbc_95@hotmail.com>
2022-08-17 19:52:57 +0200

    Format all code

--- NamespaceAdjuster/NamespaceUpdater/LogicFileNamespaceUpdaterService.cs
+++ NamespaceAdjuster/NamespaceUpdater/LogicFileNamespaceUpdaterService.cs
@@ -1,11 +1,6 @@
-﻿using Extensions;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace NamespaceBuilder
 {
